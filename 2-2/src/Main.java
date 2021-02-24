be30cabc0703c5dc93f8b798f0eb3d683ee9483a
yuya-yuya <yytyfp5115@icloud.com>
2021-02-24 12:11:49 +0900

    -additional commit

---
+++ 2-2/src/Main.java
@@ -0,0 +1,27 @@
+package study;
+
+import javax.security.auth.login.AccountException;
+
+/**
+ * Main.java
+ *
+ * 本課題では、mainメソッド・インスタンス・メソッドの呼び出しの使い方を学んでいきましょう。
+ * 課題範囲は以下のとおりです。
+ *   Study.java: 問①
+ *   Main.java: 問①から問②
+ * 指定された値と変数名を守って記述してください。
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // ① 以下のルールに従いAccountクラスのインスタンスを生成してください。
+        // 変数名:「account」
+    	Account account= new Account();
+
+        // ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
+    	account.username
+
+    }
+}
---
+++ 2-2/src/Study.java
@@ -0,0 +1,28 @@
+package study;
+
+/**
+ * Study.java
+ */
+class Account {
+
+    // フィールド変数
+    private String userName;
+    private int id;
+    private String password;
+
+    // コンストラクタ
+    public Account(String userName, int id, String password) {
+        this.userName = userName;
+        this.id = id;
+        this.password = password;
+    }
+
+    // ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
+    // アクセス修飾子:「protected」
+    // メソッド名:「printAccountInfo」
+    public printAccountInfo() {
+    	
+    }
+
+
+}
