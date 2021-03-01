f8c3f03c39c1a5668d3c8e110dca05d6702a8c07
yuya-yuya <yytyfp5115@icloud.com>
2021-03-02 00:39:33 +0900

    additional commit

--- 2-3/src/study/Task.java
+++ 2-3/src/study/Task.java
@@ -1,5 +1,28 @@
 package study;
 
-public class Task {
+// ① TaskクラスにCalculatorクラスを継承させなさい。
+public class Task extends Calculator {
 
+    /**
+     * タスクの実行
+     */
+    public void doTask() {
+
+        // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
+        // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
+
+    	protected int plus(int a) {
+            return a + 1;
+        }
+
+        protected int plus(int a, int b) {
+            return a + b;
+        }
+
+        protected int plus(int a, int b, int c) {
+            return a + b + c;
+        }
+
+    }
 }
+
