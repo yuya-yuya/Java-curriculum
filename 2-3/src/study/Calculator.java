c4f8afcc8b4b3ec4cb14571505531a9ca8d0fbdd
yuya-yuya <yytyfp5115@icloud.com>
2021-03-03 14:21:52 +0900

    add System.out.println in Main.java

--- 2-3/src/study/Calculator.java
+++ 2-3/src/study/Calculator.java
@@ -2,4 +2,16 @@ package study;
 
 public class Calculator {
 
+    protected int plus(int a) {
+        return a + 1;
+    }
+
+    protected int plus(int a, int b) {
+        return a + b;
+    }
+
+    protected int plus(int a, int b, int c) {
+        return a + b + c;
+    }
+
 }
--- 2-3/src/study/Task.java
+++ 2-3/src/study/Task.java
@@ -11,15 +11,15 @@ public class Task extends Calculator {
         // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
         // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
 
-    	protected int plus(int a) {
+    	protected int Calculatorplus(int a) {
             return a + 1;
         }
 
-        protected int plus(int a, int b) {
+        protected int Calculatorplus(int a, int b) {
             return a + b;
         }
 
-        protected int plus(int a, int b, int c) {
+        protected int Calculatorplus(int a, int b, int c) {
             return a + b + c;
         }
 
