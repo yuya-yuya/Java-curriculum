f3140699d8e61a408b05573e451eca34ed3352f7
yuya-yuya <yytyfp5115@icloud.com>
2021-03-03 18:50:32 +0900

    initial commit

---
+++ 2-4/src/study/Study.java
@@ -0,0 +1,23 @@
+package study;
+
+//②Fruits.javaをインポートしなさい。
+import fruits.Fruits;
+/**
+ * 本課題では、基本的なJavaの構造と、importについて理解を深めていきましょう。
+ *
+ * 問①〜問③まであります。
+ * 全て回答し、課題にある画像と同じ表示になるようにしてください。
+ *
+ */
+public class Study {
+
+    public static void main(String[] args) {
+
+        /* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
+         *     また、課題の画像と同じ表示になるように
+         *     printFruitsの引数にはFruitsConstantsに指定されている定数を設定してください。
+         */
+    	Fruits.printFruits("レモン",20);
+
+    }
+}
