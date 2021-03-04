fc7fd9436ed5eaf92c41830d7670916e2cce2273
yuya-yuya <yytyfp5115@icloud.com>
2021-03-04 18:46:48 +0900

    add constructor and import

--- 2-4/src/study/Study.java
+++ 2-4/src/study/Study.java
@@ -2,7 +2,6 @@ package study;
 
 //①FruitsConstants.javaをインポートしなさい。
 import constants.FruitsConstants;
-
 //②Fruits.javaをインポートしなさい。
 import fruits.Fruits;
 /**
@@ -20,7 +19,7 @@ public class Study {
          *     また、課題の画像と同じ表示になるように
          *     printFruitsの引数にはFruitsConstantsに指定されている定数を設定してください。
          */
-    	Fruits.printFruits("レモン",20);
+    	Fruits.printFruits(FruitsConstants.FRUITS_LEMON_04,FruitsConstants.FRUITS_COUNT_20);
 
     }
 }
