b1e39fc7b9994b718950fa27554db2bd29670a7d
yuya-yuya <yytyfp5115@icloud.com>
2021-03-06 00:29:24 +0900

    add commit

--- 2-3/src/study/Main.java
+++ 2-3/src/study/Main.java
@@ -14,13 +14,7 @@ public class Main {
 
         // ③ Taskクラスのインスタンスを生成し、「doTask()」メソッドを呼び出しなさい。
     	Task task = new Task();
-    	int taskvalue1 = task.Calculatorplus(10);
-    	int taskvalue2 = task.Calculatorplus(15,15);
-    	int taskvalue3 = task.Calculatorplus(20,20,20);
-
-    	System.out.println(taskvalue1);
-    	System.out.println(taskvalue2);
-    	System.out.println(taskvalue3);
+    	task.doTask();
 
     }
 
--- 2-3/src/study/Task.java
+++ 2-3/src/study/Task.java
@@ -2,7 +2,6 @@ package study;
 
 // ① TaskクラスにCalculatorクラスを継承させなさい。
 public class Task extends Calculator {
-
     /**
      * タスクの実行
      */
@@ -10,19 +9,11 @@ public class Task extends Calculator {
 
         // ② Calculator.javaのすべてのオーバーロードメソッド「plus」に適当な引数を与え、下記画像のよう出力されるようコーディングしなさい。
         // 尚、「どのクラスから呼び出しているか」を明確にするため、plus()には呼び出し元のキーワードを付与すること。
-
-    	protected int Calculatorplus(int a) {
-            return a + 1;
-        }
-
-        protected int Calculatorplus(int a, int b) {
-            return a + b;
-        }
-
-        protected int Calculatorplus(int a, int b, int c) {
-            return a + b + c;
-        }
+    	System.out.println(super.plus(10));
+    	System.out.println(super.plus(15,15));
+    	System.out.println(super.plus(20,20,20));
 
     }
 }
+
 
