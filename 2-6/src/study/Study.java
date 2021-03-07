63dac44f29665520355e1ebd770e51772421f211
yuya-yuya <yytyfp5115@icloud.com>
2021-03-08 06:07:09 +0900

    completed version

--- 2-6/src/study/Study.java
+++ 2-6/src/study/Study.java
@@ -36,6 +36,14 @@ public class Study {
         // 問③ 上記の「calendar」を使用し、本日から1年2ヶ月15日先の日付を表示しなさい。
         // - この課題に関しては課題実施日によって結果が異なりますので、Wiki課題の画像とは結果が異なります。
         // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
+        calendar.add(Calendar.YEAR,1);
+        calendar.add(Calendar.MONTH,2);
+        calendar.add(Calendar.DATE,15);
+
+        System.out.println(calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.MONTH));
+        System.out.println(calendar.get(Calendar.DATE));
+
 
 
 
