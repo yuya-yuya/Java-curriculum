1fe8d42a4613c332a60183abb2f35b723b77f542
yuya-yuya <yytyfp5115@icloud.com>
2021-03-08 06:37:13 +0900

    initial commit

---
+++ Check2　/src/check/Check.java
@@ -0,0 +1,21 @@
+package check;
+
+import Constants;
+import Pet.Pet;
+import Pet.introduce;
+
+public class Check {
+
+	public static void main(String[] args) {
+		private String firstName = "Yuya";
+		private String lastName = "Yamada";
+
+		private printName(String firstname, String lastname) {
+			System.out.plintln(firstname + lastname)
+		}
+
+	}
+
+}
+
+
