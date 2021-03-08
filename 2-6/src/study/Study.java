77457129b721efa30ce71ced77f00371d98d517a
yuya-yuya <yytyfp5115@icloud.com>
2021-03-09 01:07:04 +0900

    change month

--- 2-6/src/study/Study.java
+++ 2-6/src/study/Study.java
@@ -37,7 +37,7 @@ public class Study {
         // - この課題に関しては課題実施日によって結果が異なりますので、Wiki課題の画像とは結果が異なります。
         // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
         calendar.add(Calendar.YEAR,1);
-        calendar.add(Calendar.MONTH,2);
+        calendar.add(Calendar.MONTH,3);
         calendar.add(Calendar.DATE,15);
 
         System.out.println(calendar.get(Calendar.YEAR));
