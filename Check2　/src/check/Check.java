14f3b314d5e63ad01605d8de7469c2b1b9006a39
yuya-yuya <yytyfp5115@icloud.com>
2021-03-09 02:49:21 +0900

    additional commit

--- Check2　/src/check/Check.java
+++ Check2　/src/check/Check.java
@@ -1,21 +1,28 @@
 package check;
 
-import Constants;
-import Pet.Pet;
-import Pet.introduce;
+import constants.Constants;
+import check.Pet;
+import check.RobotPet;
 
 public class Check {
+	private String firstName = "yuya";
+	private String lastName = "yamada";
 
 	public static void main(String[] args) {
-		private String firstName = "Yuya";
-		private String lastName = "Yamada";
 
-		private printName(String firstname, String lastname) {
-			System.out.plintln(firstname + lastname)
-		}
+		Constants a = new Constants();
+		RobotPet b = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		Pet c = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+
+		System.out.println(printName(firstName, lastName));
+		c. introduce();
+		b.introduce();
 
 	}
 
-}
+	private String printName(String firstName, String lastName) {
+		return "printNameメソッド→" + lastName +firstName;
+	}
 
+}
 
