4b8a69af89275defd4557da2bfd9481280598014
yuya-yuya <yytyfp5115@icloud.com>
2021-03-10 22:15:33 +0900

    delete Constantsinstance

--- Check2　/src/check/Check.java
+++ Check2　/src/check/Check.java
@@ -8,7 +8,6 @@ public class Check {
 
 	public static void main(String[] args) {
 
-		Constants a = new Constants();
 		RobotPet b = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 		Pet c = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 
